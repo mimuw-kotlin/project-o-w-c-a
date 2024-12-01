07641891bc9be36fe05d37f2e09470293d6e937c
KrzysztofLembryk <krzysztof.lembryk@gmail.com>
2024-12-01 19:33:03 +0100

    gradlew works finally, bug was in libs.versions.toml in kotlin-plugin-serialization by default instead of version.ref = "kotlin_version", there was version = "$kotlin_version"

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "project-o-w-c-a"
---
+++ src/main/kotlin/com/example/Monitoring.kt
@@ -0,0 +1,10 @@
+package com.example
+
+import io.ktor.server.application.*
+import io.ktor.server.plugins.calllogging.*
+import io.ktor.server.request.*
+import io.ktor.server.routing.*
+import org.slf4j.event.*
+
+fun Application.configureMonitoring() {
+}
