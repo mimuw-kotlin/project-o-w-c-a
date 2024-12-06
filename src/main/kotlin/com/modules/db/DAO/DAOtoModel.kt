1f7427979d3dee030ec5252fc03c0411a3a7265c
KrzysztofLembryk <krzysztof.lembryk@gmail.com>
2024-12-06 17:48:09 +0100

    added dataModels with serialization, so that we could implmdaoToModel functions,

--- src/main/kotlin/com/modules/db/DAO/DAOtoModel.kt
+++
@@ -1,7 +0,0 @@
-package com.modules.db.DAO
-
-fun StudentDAOToModel(dao: StudentsDAO) = Task(
-    name = dao.name,
-    description = dao.description,
-    priority = Priority.valueOf(dao.priority)
-)
---
+++ src/main/kotlin/com/modules/db/DAOtoModel.kt
@@ -0,0 +1,35 @@
+package com.modules.db
+
+import com.modules.db.DAO.ClassesDAO
+import com.modules.db.DAO.StudentsDAO
+import com.modules.db.DAO.SubjectsDAO
+import com.modules.db.DAO.TeachersDAO
+import com.modules.db.dataModels.ClassModel
+import com.modules.db.dataModels.StudentModel
+import com.modules.db.dataModels.SubjectModel
+import com.modules.db.dataModels.TeacherModel
+
+fun studentDAOToModel(dao: StudentsDAO) = StudentModel(
+    index = dao.index,
+    username = dao.username,
+    user_type = dao.user_type,
+    class_nbr = dao.class_nbr
+)
+
+fun teacherDAOToModel(dao: TeachersDAO) = TeacherModel(
+    index = dao.index,
+    username = dao.username,
+    user_type = dao.user_type,
+    class_nbr = dao.class_nbr
+)
+
+fun classDAOToModel(dao: ClassesDAO) = ClassModel(
+    class_nbr = dao.class_nbr,
+    class_teacher_name = dao.class_teacher_name
+)
+
+fun subjectDAOToModel(dao: SubjectsDAO) = SubjectModel(
+    subject_code = dao.subject_code,
+    subject_name = dao.subject_name,
+    description = dao.description
+)
